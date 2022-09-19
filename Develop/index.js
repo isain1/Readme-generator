e869669f0db067c7776bf2df2dfbe24f4224f9d1
isain <ibarrais@msu.edu>
2022-09-19 05:31:45 -0400

    Added questions and initialized application.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,74 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your application?"
+    }, 
+    {
+        type: "input",
+        name: "description", 
+        message: "please write a short description of your application."
+    }, 
+    {
+        type: "input",
+        name: "installation", 
+        message: "please list instructions for installation"
+    },
+    {
+        type: "input",
+        name: "usage", 
+        message: "What is this application used for?" 
+    },
+    {
+        type: "input",
+        name: "email", 
+        message: "what is you email?"
+    },
+    {
+        type: "input",
+        name: "github", 
+        message: "what is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "contributions", 
+        message: "please list any other contributors of this application"
+    },
+    {
+        type: "list",
+        name: "license", 
+        message: "Please select one of the following licenses.",
+        choices: [
+            "Mozilla",
+            "MIT", 
+            "IBM", 
+            "ISC"
+        ]
+    },
+    {
+        type: "input",
+        name: "tests", 
+        message: "Please list any tests you would like the user to try with your application."
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers)
+            return answers
+            err ? console.log(err) : console.log("succesfully ran program")
+        })
+}
 
 // Function call to initialize app
 init();
+
